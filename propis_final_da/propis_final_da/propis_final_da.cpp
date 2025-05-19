273453db4d645ca91b16350dbde1801c93911149
Арсений <91707700+ars88809@users.noreply.github.com>
2025-05-19 13:31:38 +0300

    Update propis_final_da.cpp

--- propis_final_da/propis_final_da/propis_final_da.cpp
+++ propis_final_da/propis_final_da/propis_final_da.cpp
@@ -1,99 +1,142 @@
 #include <iostream>
-#include <cmath>
 #include <string>
+#include <cmath>
 
-std::string units[] = {"", "один", "два", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять"};
-std::string units_female[] = {"", "одна", "две", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять" };
-std::string teens[] = {"десять", "одиннадцать", "двенадцать", "тринадцать", "четырнадцать", "пятнадцать", "шестнадцать", "семнадцать", "восемнадцать", "девятнадцать" };
-std::string tens[] = {"", "", "двадцать", "тридцать", "сорок", "пятьдесят", "шестьдесят", "семьдесят", "восемьдесят", "девяносто" };
-std::string hundreds[] = {"", "сто", "двести", "триста", "четыреста", "пятьсот", "шестьсот",  "семьсот", "восемьсот", "девятьсот" };
-std::string forms[] = {"", "тысяча", "тысячи", "тысяч", "миллион", "миллиона", "миллионов", "миллиард", "миллиарда", "миллиардов" };
+std::string triplet(int num);
+std::string thousands_triplet(int num);
+std::string getThousandForm(int num);
+std::string getLargeForm(int num, int level);
+std::string numToWord(int num);
 
-std::string triplet(int num, bool female);
-std::string num_to_word(long long num);
-std::string getForm(int n, int num_seg);
+const char* numbers[4][10] = {
+    {"", "один", "два", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять"},         
+    {"десять", "одиннадцать", "двенадцать", "тринадцать", "четырнадцать", "пятнадцать", "шестнадцать", "семнадцать", "восемнадцать", "девятнадцать"},
+    {"", "", "двадцать", "тридцать", "сорок", "пятьдесят", "шестьдесят", "семьдесят", "восемьдесят", "девяносто"},
+    {"", "сто", "двести", "триста", "четыреста", "пятьсот", "шестьсот", "семьсот", "восемьсот", "девятьсот"}
+};
+
+const char* thousands_forms[5][10] = {
+    {"", "одна", "две", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять"},     
+    {"десять", "одиннадцать", "двенадцать", "тринадцать", "четырнадцать", "пятнадцать", "шестнадцать", "семнадцать", "восемнадцать", "девятнадцать"},
+    {"", "", "двадцать", "тридцать", "сорок", "пятьдесят", "шестьдесят", "семьдесят", "восемьдесят", "девяносто"},
+    {"", "сто", "двести", "триста", "четыреста", "пятьсот", "шестьсот", "семьсот", "восемьсот", "девятьсот"},
+    { "тысяч", "тысяча", "тысячи" }
+};
+
+const char* large_forms[2][3] = {
+    {"миллионов", "миллион", "миллиона"},  
+    {"миллиардов", "миллиард", "миллиарда"} 
+};
 
 int main()
 {
     setlocale(LC_ALL, "");
-    long long n;
-    while (true) 
-    {
-        std::cout << "Введите число: ";
-        std::cin >> n;
-        std::cout << num_to_word(n) << std::endl;
-    }
-    return 0;
+    long long numb;
+    std::cin >> numb;
+    std::cout << numToWord(numb);
 }
 
-std::string triplet(int num, bool female) {
-    std::string result;
-    int h = num / 100;
-    int t = (num / 10) % 10;
-    int u = num % 10;
-
-    if (h > 0) {
-        result += hundreds[h] + " ";
-    }
-
-    if (t == 1) {
-        result += teens[u] + " ";
-    }
-    else {
-        if (t > 1) {
-            result += tens[t] + " ";
-        }
-        if (u > 0) {
-            result += (female ? units_female[u] : units[u]) + " ";
-        }
-    }
-
-    return result;
-}
-
-std::string getForm(int n, int num_seg)
+std::string triplet(int num)
 {
-    int seg = (num_seg - 1) * 3 + 1;
-    n %= 100;
-    if (n >= 11 && n <= 19)
+    std::string sbor;
+    if ((num / 10) % 10 == 1)
     {
-        return *(forms + seg + 2);
+        sbor += numbers[3][num / 100 % 10];
+        sbor += " ";
+        sbor += numbers[1][num % 10];
     }
-    switch (n % 10) 
+    else
     {
-    case 1: return *(forms + seg);
-    case 2:
-    case 3:
-    case 4: return *(forms + seg + 1);
-    default: return *(forms + seg + 2);
+        sbor += numbers[3][num / 100 % 10];
+        sbor += " ";
+        sbor += numbers[2][num / 10 % 10];
+        sbor += " ";
+        sbor += numbers[0][num % 10];
     }
+    return sbor;
 }
 
-std::string num_to_word(long long num)
+std::string thousands_triplet(int num)
 {
-    std::string result;
-    int len = (log10(num) + 1);
-    int segments = (len + 2) / 3;
-    for (int seg = segments - 1; seg >= 0; seg--) {
-        long long power = pow(1000, seg);
-        int current_segment = (num / power) % 1000;
-        if (current_segment > 0) {
-            bool female = (seg == 1); 
-            result += triplet(current_segment, female);
-            if (seg > 0) {
-                std::string form = getForm(current_segment, seg);
-                if (!form.empty()) {
-                    result += form + " ";
-                }
+    std::string sbor;
+    if ((num / 10) % 10 == 1)
+    {
+        sbor += thousands_forms[3][num / 100 % 10];
+        sbor += " ";
+        sbor += thousands_forms[1][num % 10];
+    }
+    else
+    {
+        sbor += thousands_forms[3][num / 100 % 10];
+        sbor += " ";
+        sbor += thousands_forms[2][num / 10 % 10];
+        sbor += " ";
+        sbor += thousands_forms[0][num % 10];
+    }
+    sbor += " " + getThousandForm(num);
+    return sbor;
+}
+
+std::string getThousandForm(int num) {
+    int last_two = num % 100;
+    int last_one = num % 10;
+
+    int index = (last_two >= 11 && last_two <= 14) ? 0 : (last_one == 1 ? 1 : (last_one >= 2 && last_one <= 4 ? 2 : 0));
+
+    return thousands_forms[4][index];
+}
+
+std::string getLargeForm(int num, int level) {
+    int last_two = num % 100;
+    int last_one = num % 10;
+
+    int index = (last_two >= 11 && last_two <= 14) ? 0 : (last_one == 1 ? 1 : (last_one >= 2 && last_one <= 4 ? 2 : 0));
+
+    return large_forms[level][index];
+}
+
+std::string numToWord(int num)
+{
+
+    std::string sbor;
+    int temp = num;
+    int seg = 0;
+    while (temp > 0) 
+    {
+        temp /= 1000;
+        seg++;
+    }
+
+    for (int parts = seg; parts > 0; parts--)
+    {
+        long long power = 1;
+        for (int i = 1; i < parts; i++)
+        {
+            power *= 1000;
+        }
+
+        int current_part = (num / power) % 1000;
+        if (current_part == 0) continue;
+
+        if (parts == 2)
+        { 
+            sbor += thousands_triplet(current_part);
+        }
+        else 
+        {
+            sbor += triplet(current_part);
+            if (parts > 2)
+            {
+                sbor +=  ' ' + getLargeForm(current_part, abs(parts - 4));
             }
         }
-
+        sbor += " ";
     }
 
-    // Удаляем лишний пробел в конце
-    if (!result.empty() && result.back() == ' ') {
-        result.pop_back();
-    }
 
-    return result;
+    while (!sbor.empty() && sbor.back() == ' ') sbor.pop_back();
+    while (!sbor.empty() && sbor.front() == ' ') sbor.erase(0, 1);
+
+
+    return sbor;
 }
